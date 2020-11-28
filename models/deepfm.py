df0571e6447bb1d53a480bf9ddb8377c8a8bc489
stephenbarrnet <stephenbarrnet@gmail.com>
2020-11-28 21:22:36 +0800

    update

--- models/deepfm.py
+++ models/deepfm.py
@@ -3,7 +3,7 @@
 Author:
     stephenbarrnet@gmail.com
 Reference:
-    [1] Guo H, Tang R, Ye Y, et al. Deepfm: a factorization-machine based neural network for ctr prediction[J]. arXiv preprint arXiv:1703.04247, 2017.(https://arxiv.org/abs/1703.04247)
+    [1] Guo , Tang R, Ye Y, et al. Deepfm: a factorization-machine based neural network for ctr prediction[J]. arXiv preprint arXiv:1703.04247, 2017.(https://arxiv.org/abs/1703.04247)
 """
 import torch
 import torch.nn as nn
