d4aea14ff51e4d81988b12d3c78b2392937c3a53
stephenbarrnet <stephenbarrnet@gmail.com>
2021-03-16 15:34:15 +0800

    update

---
+++ models/dsin.py
@@ -0,0 +1,35 @@
+#  Copyright © 2020-2020 Hailin Fu All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ==============================================================================
+
+
+"""
+Author:
+    Hailin Fu, hailinfufu@outlook.com
+"""
+from deepray.model.model_ctr import BaseCTRModel
+
+
+class DeepSessionInterestNetwork(BaseCTRModel):
+    def __init__(self):
+        super(DeepSessionInterestNetwork, self).__init__()
+
+    def build_network(self, features, is_training=None):
+        """
+        TODO
+
+        :param features:
+        :param is_training:
+        :return:
+        """
